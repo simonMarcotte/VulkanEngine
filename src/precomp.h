6bfa4d40dd2b5b1e4dbadb62800a35fb6820a13e
Simon <54853018+simonMarcotte@users.noreply.github.com>
2025-02-04 21:51:02 -0700

    precompilation header file

---
+++ src/precomp.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+#include <cmath>
+#include <cstdlib>
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <glm/glm.hpp>
+#include <gsl/gsl>
+#include <utilities.h>
