34a059abb2ca4b0a5bd91e101ddea3fe09786159
Simon <54853018+simonMarcotte@users.noreply.github.com>
2025-02-08 21:14:28 -0700

    glfw callback function

--- src/glfw_initialization.cpp
+++ src/glfw_initialization.cpp
@@ -2,12 +2,19 @@
 #include <cstdlib>
 #include <GLFW/glfw3.h>
 #include <precomp.h>
+#include <spdlog/spdlog.h>
 
 namespace veng {
+
+    void glfw_error_callback(std::int32_t error_code, gsl::czstring message){
+        spdlog::error("GFLW Validation: {}", message);
+    }
+
     GlfwInitilization::GlfwInitilization() { 
+        glfwSetErrorCallback(glfw_error_callback);
         if (glfwInit() != GLFW_TRUE) 
             std::exit(EXIT_FAILURE);
-        }
+    }
 
     GlfwInitilization::~GlfwInitilization() {
         glfwTerminate();
